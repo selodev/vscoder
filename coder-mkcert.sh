7dbf5db62efeecf6e1d75cf28316bc07bcbba433
selodev <selahattin.developer@gmail.com>
2021-02-26 05:05:44 +0000

    updated dockerfile

--- coder-mkcert.sh
+++ coder-mkcert.sh
@@ -1,2 +1,4 @@
 CURRENT_UID=$(id -u):$(id -g) \
+U_UID=$(id -u) \
+U_GID=$(id -g) \
 docker-compose -f docker-compose.local.yaml up "$@"
--- coder/docker-entrypoint.sh
+++ coder/docker-entrypoint.sh
@@ -7,12 +7,12 @@ set -ea
 # WARNING: systemd not found. You have to remove XDG_RUNTIME_DIR manually on every logout.
 
 #PATH=/home/coder/bin:/sbin:/usr/sbin:$PATH
-export XDG_RUNTIME_DIR=/home/coder/.docker/run
-export PATH=/home/coder/bin:$PATH
-export DOCKER_HOST=unix:///home/coder/.docker/run/docker.sock
+export XDG_RUNTIME_DIR=/home/${DOCKER_USER}/.docker/run
+export PATH=/home/${DOCKER_USER}/bin:$PATH
+export DOCKER_HOST=unix:///home/${DOCKER_USER}/.docker/run/docker.sock
 
 
-export PATH=/home/coder/bin:/sbin:/usr/sbin:$PATH 
+export PATH=/home/${DOCKER_USER}/bin:/sbin:/usr/sbin:$PATH 
 
 
 
@@ -22,27 +22,12 @@ export PATH=/home/coder/bin:/sbin:/usr/sbin:$PATH
 #dumb-init dockerd-rootless.sh
 
 
-export PATH=/home/coder/.local/bin:$PATH
+export PATH=/home/${DOCKER_USER}/.local/bin:$PATH
 #unshare -m -U /bin/bash
 #nsenter -U --preserve-credentials -n -m -t $(cat $XDG_RUNTIME_DIR/docker.pid)
 
 
-# We do this first to ensure sudo works below when renaming the user.
-# Otherwise the current container UID may not exist in the passwd database.
-eval "$(fixuid -q)"
-
-if [ "${DOCKER_USER-}" ] && [ "$DOCKER_USER" != "$USER" ]; then
-  echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/nopasswd > /dev/null
-  # Unfortunately we cannot change $HOME as we cannot move any bind mounts
-  # nor can we bind mount $HOME into a new home as that requires a privileged container.
-  sudo usermod --login "$DOCKER_USER" coder
-  sudo groupmod -n "$DOCKER_USER" coder
-
-  USER="$DOCKER_USER"
-  echo "$DOCKER_USER" 'docker user'
-  sudo sed -i "/coder/d" /etc/sudoers.d/nopasswd
-fi
-
+#  sudo chown -R $USER $HOME
 
 dumb-init code-server "$@"
 
