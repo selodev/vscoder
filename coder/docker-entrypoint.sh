b9fa28f59c5b64e8c5308215a9e501caf7367dd0
selodev <selahattin47usa@hotmail.com>
2021-03-02 23:57:00 +0000

    updates

--- coder/docker-entrypoint.sh
+++ coder/docker-entrypoint.sh
@@ -18,7 +18,21 @@ export PATH=/home/${USER}/bin:/sbin:/usr/sbin:$PATH
 
 PATH=/home/${USER}/bin:/sbin:/usr/sbin:$PATH
 
+# We do this first to ensure sudo works below when renaming the user.
+# Otherwise the current container UID may not exist in the passwd database.
+eval "$(fixuid -q)"
 
+if [ "${DOCKER_USER-}" ] && [ "$DOCKER_USER" != "$USER" ]; then
+  echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/nopasswd > /dev/null
+  # Unfortunately we cannot change $HOME as we cannot move any bind mounts
+  # nor can we bind mount $HOME into a new home as that requires a privileged container.
+  sudo usermod --login "$DOCKER_USER" coder
+  sudo groupmod -n "$DOCKER_USER" coder
+
+  USER="$DOCKER_USER"
+
+  sudo sed -i "/${DOCKER_USER}/d" /etc/sudoers.d/nopasswd
+fi
 #dumb-init dockerd-rootless.sh
 
 
